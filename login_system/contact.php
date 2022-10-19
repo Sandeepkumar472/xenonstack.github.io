bf7fd4eefb49de5ebc691c65e79776764085cd19
sandeep <sandeepkumar49311@gmail.com>
2022-10-19 22:34:30 +0530

    web page using html ccss php and database

---
+++ login_system/contact.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="./assets/css/contactstyle.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body style="background-image: url(https://source.unsplash.com/650x550)">
+    <div class="container">
+        <header>
+            <h1>Contact Us</h1>
+            <p>SmartTech is a platform to get the Smart Accessories & Look like a Smart Person</p>
+        </header>
+        <div class="content">
+            <div class="content-form">
+                <section>
+                    <i class="fa fa-map-marker fa-2x" aria-hidden="true"></i>
+                    <h2>address</h2>
+                    <p>
+                        1st flore, Gupta Apartment,<br>Near Sachdeva College of Pharmacy<br>Gharuan, Kharar, Mohali<br> Punjab, 140413
+                    </p>
+                </section>
+
+                <section>
+                    <i class="fa fa-phone fa-2x" aria-hidden="true"></i>
+                    <h2>Phone</h2>
+                    <p>+91 9939963986<br>+91 7004795819</p>
+                </section>
+
+                <section>
+                    <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                    <h2>E-mail</h2>
+                    <p>bgupta535@hotmail.com<br>bibhanshug72@gmail.com</p>
+                </section>
+            </div>
+        </div>
+
+      <form>
+        <div class="form">
+            <div class="right">
+              <div class="contact-form">
+                  <input type="text" name="name" required>
+                  <span>Full Name</span>
+              </div>
+  
+              <div class="contact-form">
+                  <input type="E-mail" name="email" required>
+                  <span>E-mail Id</span>
+              </div>
+              <div class="contact-form">
+                  
+                    
+                  </textarea>
+                  <input type="text" name="msg">
+                  <span> Type your Message....</span>
+              </div>
+  
+              <div class="contact-form">
+                  <input type="submit" name="submit">
+              </div>
+              </div>
+            </div>
+          </div>       
+    </form>
+    <div class="media">
+        <li><a href="https://www.facebook.com/" class="fa fa-facebook-square fa-2x"></a></li>
+        <li><a href="https://www.instagram.com/" class="fa fa-instagram fa-2x"></a></li>
+        <li><a href="https://web.whatsapp.com/" class="fa fa-whatsapp fa-2x"></a></li>
+        <li><a href="https://twitter.com/i/flow/login" class="fa fa-twitter-square fa-2x"></a></li>
+    </div>
+    <div class="empty">
+
+    </div>
+</div>    
+</body>
+</html>
